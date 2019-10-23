0234016dc871ec7451a6654b061ea741e9d9fa51
Yichun Wu <kobo1998@live.com>
2019-10-23 10:33:44 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -6,21 +6,23 @@ var multer = require('multer');
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 var cassandra = require('cassandra-driver');
 var upload = multer({ dest: 'uploads/' })
+var fs = require('fs')
 
 router.post('/',upload.single('contents'),function(req,res){
     var data = req.body
-    var query = 'INSERT INTO imgs (filename, contents) VALUES (?, ?)'; 
-    var params = [data.filename, req.file.buffer];
     client = req.app.locals.client
-    console.log(req.file.buffer);
-    client.execute(query, params, { prepare: true }, function(err){
-        if(err){
-            console.log(err);
-            res.json({status:"error"});
-        }else{
-            console.log("INSERT "+data.filename)
-            res.json({status:"OK"});
-        }
+    fs.readFile(req.file.path,function(err, contents){
+        var query = 'INSERT INTO imgs (filename, contents) VALUES (?, ?)'; 
+        var params = [data.filename, contents];
+        client.execute(query, params, { prepare: true }, function(err){
+            if(err){
+                console.log(err);
+                res.json({status:"error"});
+            }else{
+                console.log("INSERT "+data.filename)
+                res.json({status:"OK"});
+            }
+        })
     })
 })
 
