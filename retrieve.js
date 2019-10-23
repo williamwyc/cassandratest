74b04868c4145a3f57dc842a6268c312d6c18820
Yichun Wu <kobo1998@live.com>
2019-10-23 10:17:08 -0400

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -9,6 +9,7 @@ var cassandra = require('cassandra-driver');
 router.get('/',jsonParser,function(req,res){
     var query = 'SELECT contents FROM imgs WHERE filename = ?'; 
     var params = [req.query.filename];
+    console.log(req.query.filename)
     client = req.app.locals.client
     client.execute(query, params, function(err, result){
         if(err){
@@ -16,8 +17,10 @@ router.get('/',jsonParser,function(req,res){
             res.json({status:"error"});
         }else{
             type = 'image/'+req.query.filename.split('.')[1]
-            res.type(type)
-            res.send(result.first().contents)
+            console.log(type)
+            res.setHeader('Content-Type', type)
+            res.write(result.first().contents)
+            res.end()
         }
     })
 })
